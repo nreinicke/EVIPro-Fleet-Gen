ccb1b8aacf73c4d1093a21dffcf0a01b51dcea11
HAGRID <hagrid@hagrid.hagrid>
2020-01-25 13:19:47 -0800

    Jerome: corrected error where evi_load_profiles_suv was loading load profiles for sedans. Factored the class_type data columns. Cleaned up a bit.

--- functions/loadRawData.R
+++ functions/loadRawData.R
@@ -1,4 +1,6 @@
 loadRawData <- function(temp) {
+					
+					# Get EVI-Pro charge session data -------------------------------------------------------
 					evi_raw <- readRDS(paste0("../../data/preprocessed/evi_raw/", temp, ".rds"))
 					# add column for vehicle class
 					evi_raw[, "class_type" := "Sedan"]
@@ -11,29 +13,24 @@ loadRawData <- function(temp) {
 					
 					# add one hundred million to the unique vids to distinguish from sedan data
 					evi_raw_suv[, unique_vid := unique_vid + 100000000]
-					
-					# create empty data.table
-					# evi_raw_dt <- data.table()
-					
-					# bind together into a single data.table
-				 #	evi_raw_dt <- rbind(evi_raw_suv,evi_raw)
 				
-					# list approach
+					# Bind sedan and suv data into a list
 					evi_raw_dt <- rbindlist(list(evi_raw, evi_raw_suv))
+					
+					#Factor class_type
+					evi_raw_dt$class_type <- factor(evi_raw_dt$class_type,levels=c("Sedan","SUV"))
 				
-				#	return(evi_raw_dt)
-				gc(rm(evi_raw_suv, evi_raw))
-				# load the load profiles
-				# loadprof_data_path_sedan <- paste0("../../data/preprocessed/load_profile/", temp_vec[i], ".rds")
-				# evi_load_profiles <- readRDS(loadprof_data_path_sedan)
-				
+					#	Garbage collection
+					gc(rm(evi_raw_suv, evi_raw))
+					
+					# Get pre-calculated load profiles --------------------------------------------------------
 					evi_load_profiles <- readRDS(paste0("../../data/preprocessed/load_profile/", temp, ".rds"))
 
 					# add vehicle class
 					evi_load_profiles[, "class_type" := "Sedan"]
 
 					# Repeat for SUVs
-					evi_load_profiles_suv <- readRDS(paste0("../../data/preprocessed/load_profile/", temp, ".rds"))
+					evi_load_profiles_suv <- readRDS(paste0("../../data/preprocessed/load_profile_suv/", temp, ".rds"))
 
 					# add vehicle class
 					evi_load_profiles_suv[, "class_type" := "SUV"]
@@ -41,16 +38,15 @@ loadRawData <- function(temp) {
 					# add one hundred million to unique vid to distinguish SUV class of vehicle
 					evi_load_profiles_suv[, unique_vid := unique_vid + 100000000]
 
-					# create empty data.table
-					#evi_load_profiles_dt <- data.table()
-
-					# bind together into a single data.table
-					# evi_load_profiles_dt <- rbind(evi_load_profiles_suv,
-					# 																											evi_load_profiles)
+					# Bind sedan and suv data into a list
 					evi_load_profiles_dt <- rbindlist(list(evi_load_profiles, evi_load_profiles_suv))
-
-
+					
+					#Factor class_type
+					evi_load_profiles_dt$class_type <- factor(evi_load_profiles_dt$class_type,levels=c("Sedan","SUV"))
+					
+					#Garbage collection
 				 gc(rm(evi_load_profiles, evi_load_profiles_suv))
 
+				 # Return loaded data ----------------------------------------------------------------------
 					return(list(evi_raw_dt, evi_load_profiles_dt))
 }
