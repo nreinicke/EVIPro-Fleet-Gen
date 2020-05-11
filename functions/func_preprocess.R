d568d02d540be814e5fb72121845622130f68c92
HAGRID <hagrid@hagrid.hagrid>
2020-05-11 08:50:14 -0700

    Jerome: move data loading function to .Rmd, edit comments.

--- functions/func_preprocess.R
+++ functions/func_preprocess.R
@@ -1,58 +1,48 @@
 # Author: Schatz Energy Research Center
 # Original Version: Micah Wright
 # Date: March 01, 2019
-# Description: Processes and saves the evi sessions and time series load profile 
-#   for all ambient temoperatures. This is not designed to be an interactive 
-#   function, timestep is assumed to be 15 minutes in all cases.
-
-#######################
-# Edits: by Max Blasdel
-# Date: 9/12/19
-# Purpose: Re-run this function on additional SUV data from NREL
-# Changes: changing data sources and file save locations
-# changes mark with `# suv` where appropriate
-#######################
-
-#######################
-# NOTE: Since this was written the RProj file had been accidentaly erased
-# This was a product of issues with the git projects (we think)
-# The working directory is now required to be set for all function calls
-
-# set wd
-# setwd("/media/spin/sein-evi/source/NREL-Demo_Project")
-#######################
+# Description: Processes and saves the evi sessions and time series load profile for all ambient temperatures.
+# Version History
+#			2019-09-12 Max: minor edits, run on SUV data
+#			2020-05-04 Jerome: turn into formal function for use in Gen-Preprocessed-Data.Rmd in order to formalize and document all data generation steps
 
 # load necessary packages
 library(future.apply)
 library(data.table)
 
 # source functions
-# loads EVIPro data and stores in a single data table
-source("functions/func_LoadEVIPro.R") 
+source("functions/func_LoadEVIPro.R") # loads EVIPro data and stores in a single data table
+source("functions/func_calcBaseEVILoad.R") # pre-calculates load profile for all unique_vid in evi_raw
 
-# pre-calculates load profile for all unique_vid in evi_raw
-source("functions/func_calcBaseEVILoad.R") 
+#Define function
+preprocess_NREL_data <- function(inputdir_evipro,        # character string of directory containing files provided by NREL
+																																	inputdir_chts,          # character string of file path to chts_dvmt.csv file provided by NREL
+																																	outputdir_eviraw,       # character string of directory to save raw evi .rds files
+																																	outputdir_loadprofile,  # character string of directory to save raw load profile .rds files
+																																	temp_list,              # vector of character strings of temperatures to process (i.e. c("20C","30C","35C"))
+																																	vmt_bin_size,           # integer of vmt bin size for load_EVIPro() function
+																																	loadprofile_timestep) { # float of time step in decimal hours for calcBaseEVILoad() function
+		
+		# Parallelize lapply across temperatures
+		future_lapply(seq(1:length(temp_list)), function(i) {
+		  # load charging session data into data table
+		  evi_raw <- load_EVIPro(inputdir_evipro,
+		                         temp_list[i], 
+		  																							inputdir_chts,
+		  																							vmt_bin_size)
+		  
+		  # Save charging session data table
+		  saveRDS(evi_raw, paste0(outputdir_eviraw,
+		                          temp_list[i], 
+		                          ".rds"))
+		  
+		  # Create load profiles
+		  evi_load_profiles <- calcBaseEVILoad(evi_raw, loadprofile_timestep)
+		  
+		  # Save load profiles data table
+		  saveRDS(evi_load_profiles, paste0(outputdir_loadprofile,
+		                          										temp_list[i], 
+		                          										".rds"))
+		})
 
-# set up par
-plan(multiprocess, workers = 6)
-
-temp_list <- seq(-20, 40, 5)
-
-temp_list <- paste0(temp_list, "C")
-
-future_lapply(seq(1:length(temp_list)), function(i) {
-  # load the raw data
-  evi_raw <- load_EVIPro("../../data/sessions_all_ambient_temps_suv/", # suv
-                        temp_list[i], 
-                         "../../data/chts_dvmt.csv", # path change
-                         10)
-  saveRDS(evi_raw, paste0("../../data/preprocessed/evi_raw_suv/", # suv
-                          temp_list[i], 
-                          ".rds"))
-  
-  evi_load_profiles <- calcBaseEVILoad(evi_raw, 0.25)
-  
-  saveRDS(evi_load_profiles, paste0("../../data/preprocessed/load_profile_suv/", # suv
-                          temp_list[i], 
-                          ".rds"))
-})
+}
