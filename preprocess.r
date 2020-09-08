2e0faab8f4e3f0b02db60921c07bf183e58e8dd7
Hoshiko <jhoshiko@nrel.gov>
2020-09-08 11:23:49 -0600

    Updated script to run all combinatins of work and home load shift strategies

--- preprocess.r
+++ preprocess.r
@@ -14,30 +14,42 @@ source("functions/func_loadEVIPro.R") #loads raw EVIPro model output data and st
 temp_vec <- seq(-20,40, 10)
 temp_vec <- paste0(temp_vec, "C")
 
-# Sedans: nothing returned. Results saved in specified output directories
-#print("working on sedan data")
-#preprocess_NREL_data(temp_list = temp_vec,
-#                     inputdir_evipro = "input/EVIPro_sessions_all_ambient_temps_sedan/",
-#                     inputdir_chts = "input/chts_dvmt.csv",
-#                     outputdir_eviraw = "input/preprocessed/evi_raw_sedan/",
-#                     outputdir_loadprofile = "input/preprocessed/load_profile_sedan/",
-#                     vmt_bin_size = 10, # bin width in miles of fleet dvmt distribution. Must be an integer
-#                     loadprofile_timestep = 0.25, # time step in decimal hours of electricity demand profile
-#                     work_load_shift = "max_delay", # string of load shift strategy 
-#                     home_load_shift = "max_delay", # string of load shift strategy 
-#                     temp_group_size = 4, # The number of temperatures that the program will work on at a time
-#                     desired_time = 1.0) # desired time specifically for the timed charging load shift strategy
+work_shift_vec <- c("min_delay", "max_delay", "load_leveling")
+home_shift_vec <- c("min_delay", "max_delay", "load_leveling", "timed_charging")
 
-# SUVs: nothing returned. Results saved in specified output directories
-print("working on SUV data")
-preprocess_NREL_data(temp_list = temp_vec,
-                     inputdir_evipro = "input/EVIPro_sessions_all_ambient_temps_suv/",
-                     inputdir_chts = "input/chts_dvmt.csv",
-                     outputdir_eviraw = "input/preprocessed/evi_raw_suv/",
-                     outputdir_loadprofile = "input/preprocessed/load_profile_suv/",
-                     vmt_bin_size = 10, # bin width in miles of fleet dvmt distribution
-                     loadprofile_timestep = 0.25, # time step in decimal hours of electricity demand profile
-                     work_load_shift = "load_leveling", # string of load shift strategy 
-                     home_load_shift = "timed_charging", # string of load shift strategy 
-                     temp_group_size = 4, # The number of temperatures that the program will work on at a time
-                     desired_time = 1.0) # desired time specifically for the timed charging load shift strategy
+for(work_shift in 1:length(work_shift_vec)){
+  for(home_shift in 1:length(home_shift_vec)) {
+    
+    print(paste0("Working on load shift combination: work-", work_shift_vec[work_shift], " home-", home_shift_vec[home_shift]))
+    
+    # Sedans: nothing returned. Results saved in specified output directories
+    print("working on sedan data")
+    preprocess_NREL_data(temp_list = temp_vec,
+                        inputdir_evipro = "input/EVIPro_sessions_all_ambient_temps_sedan/",
+                        inputdir_chts = "input/chts_dvmt.csv",
+                        outputdir_eviraw = "input/preprocessed/evi_raw_sedan/",
+                        outputdir_loadprofile = "input/preprocessed/load_profile_sedan/",
+                        vmt_bin_size = 10, # bin width in miles of fleet dvmt distribution. Must be an integer
+                        loadprofile_timestep = 0.25, # time step in decimal hours of electricity demand profile
+                        work_load_shift = work_shift_vec[work_shift], # string of load shift strategy
+                        home_load_shift = home_shift_vec[home_shift], # string of load shift strategy
+                        temp_group_size = 4, # The number of temperatures that the program will work on at a time
+                        desired_time = 1.0) # desired time specifically for the timed charging load shift strategy
+    
+    # SUVs: nothing returned. Results saved in specified output directories
+    print("working on SUV data")
+    preprocess_NREL_data(temp_list = temp_vec,
+                         inputdir_evipro = "input/EVIPro_sessions_all_ambient_temps_suv/",
+                         inputdir_chts = "input/chts_dvmt.csv",
+                         outputdir_eviraw = "input/preprocessed/evi_raw_suv/",
+                         outputdir_loadprofile = "input/preprocessed/load_profile_suv/",
+                         vmt_bin_size = 10, # bin width in miles of fleet dvmt distribution
+                         loadprofile_timestep = 0.25, # time step in decimal hours of electricity demand profile
+                         work_load_shift = work_shift_vec[work_shift], # string of load shift strategy
+                         home_load_shift = home_shift_vec[home_shift], # string of load shift strategy
+                         temp_group_size = 4, # The number of temperatures that the program will work on at a time
+                         desired_time = 1.0) # desired time specifically for the timed charging load shift strategy
+  }
+  
+}
+
